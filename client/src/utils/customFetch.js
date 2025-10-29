237a8078c2a4d3c4acc82918f470439fa12b4b36
mishika7608 <csds23040@glbitm.ac.in>
2025-10-29 19:37:35 +0530

    making changes to customFetch and index.js of components

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,17 +1,7 @@
-// import axios from 'axios';
-
-// const customFetch = axios.create({
-//     baseURL:'/api/v1',
-// })
-
-// export default customFetch;
-
-
 import axios from 'axios';
 
-// Use relative path so Vite proxy works
 const customFetch = axios.create({
-  baseURL: '/api', // ✅ proxy will handle this
+  baseURL: '/api/v1',
 });
 
 export default customFetch;
