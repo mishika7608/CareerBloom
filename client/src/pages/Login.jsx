7c72d94de9cea4a50287a85bc4075baa89cb05a0
mishika7608 <csds23040@glbitm.ac.in>
2025-10-29 18:00:11 +0530

    Editing the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,51 +1,60 @@
-import { Form , Link, redirect, useActionData, useNavigation } from 'react-router-dom';
-import React from 'react';
+import { Link, Form, redirect, useNavigate } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
-import {Logo, FormRow} from '../components';
+import { FormRow, Logo, SubmitBtn } from '../components';
 import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
-export const action = async ({ request }) => {
-  const formData = await request.formData()
-  const data = Object.fromEntries(formData)
-  const errors = { msg: ''};
-  if (data.password.length < 5){
-    errors.msg = 'password too short';
-    return errors;
-  }
-  try{
-    await customFetch.post('/v1/auth/login',data)
-    return redirect('/dashboard');
-  } catch(error){
-    // errors.msg = error?.response?.data?.msg;
-    console.log(error)
-    return errors;
-  }
-  
-}
-
+export const action =
+  (queryClient) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    try {
+      await customFetch.post('/auth/login', data);
+      queryClient.invalidateQueries();
+      toast.success('Login successful');
+      return redirect('/dashboard');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return error;
+    }
+  };
 
 const Login = () => {
-  const errors = useActionData();
-  const navigation = useNavigation()
-  const isSubmitting = navigation.state === 'submitting'; 
+  const navigate = useNavigate();
+
+  const loginDemoUser = async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'secret123',
+    };
+    try {
+      await customFetch.post('/auth/login', data);
+      toast.success('Take a test drive');
+      navigate('/dashboard');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+  };
   return (
     <Wrapper>
-      <Form method='post'className='form'>
+      <Form method='post' className='form'>
         <Logo />
-        <h4>Login</h4>
-        {errors?.msg && <p style={{color:'red' }}>{errors.msg}</p>}
-        <FormRow type="email" name="email" defaultValue='john@gmail.com'/>
-        <FormRow type="password" name="password" defaultValue='secret123'/>
-        <button type='submit' className='btn btn-block' disabled={isSubmitting}>{isSubmitting?'Submitting': 'Submit'}</button>
-        <button type='submit' className='btn btn-block'>
+        <h4>login</h4>
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
+        <SubmitBtn />
+        <button type='button' className='btn btn-block' onClick={loginDemoUser}>
           explore the app
         </button>
         <p>
           Not a member yet?
-          <Link to='/register' className='member-btn'>Register</Link>
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
         </p>
       </Form>
     </Wrapper>
-  )
-}
+  );
+};
 export default Login;
